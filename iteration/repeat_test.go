a32042be875ac6b07391b82adfcb04426f834bc8
Garrett <duldonkulus@gmail.com>
2026-01-13 21:39:34 -0600

    Add iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,20 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	sum := Repeat("z", 3)
+	fmt.Println(sum)
+}
